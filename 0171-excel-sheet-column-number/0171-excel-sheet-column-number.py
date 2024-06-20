6b5b04112bfecb81ba60808531ed8152d5811172
100baek-go <163807417+100baek-go@users.noreply.github.com>
2024-06-21 00:02:12 +0900

    Time: 35 ms (68.78%), Space: 16.5 MB (25.78%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        s = columnTitle[::-1]
+        return sum([(ord(s[i]) - 64) * (26 ** i) for i in range(len(s))])
+        
