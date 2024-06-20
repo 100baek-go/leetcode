adc4acdf4254202ea819e4064a86caaa0b01b8ee
100baek-go <163807417+100baek-go@users.noreply.github.com>
2024-06-21 00:08:25 +0900

    Time: 376 ms (85.95%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,10 +1,2 @@
-import pandas as pd
-
-def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    df=pd.DataFrame(employee)
-    df.rename(columns={'name': 'Employee'}, inplace=True)
-    result = pd.merge(df, df, left_on='managerId', right_on='id', suffixes=('', '_manager'))
-    result = result[result['salary'] > result['salary_manager']]
-    result = result[['Employee']]
-    return result
-    
+select e1.name as employee from employee e1
+join employee e2 on e2.id=e1.managerid and e1.salary> e2.salary
