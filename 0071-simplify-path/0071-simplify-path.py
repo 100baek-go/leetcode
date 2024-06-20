d816ea1e94eb55283554572da0abb37f9a8f6078
100baek-go <163807417+100baek-go@users.noreply.github.com>
2024-06-20 23:39:36 +0900

    Time: 37 ms (56.35%), Space: 16.8 MB (8.58%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,20 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.replace('//', '/')
+        path_stack = deque()
+        for sub_path in path.split('/'):
+            if sub_path:
+                path_stack.append(sub_path)
+        new_path_stack = deque()
+        up_dir = 0
+        while path_stack:
+            sub_path = path_stack.pop()
+            if sub_path == '..':
+                up_dir += 1
+                continue
+            if sub_path != '.' and sub_path != '..' and up_dir == 0:
+                new_path_stack.appendleft(sub_path)
+            elif up_dir != 0 and sub_path != '.':
+                up_dir -= 1
+        new_path = '/'.join(new_path_stack)
+        return f"/{new_path}"
