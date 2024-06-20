8c86b0bb7bc49f01e6136d120c71d281ea73a2b0
100baek-go <163807417+100baek-go@users.noreply.github.com>
2024-06-21 02:32:23 +0900

    Update 0577-employee-bonus.py

--- 0577-employee-bonus/0577-employee-bonus.py
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
-    
+    df = pd.merge(left=employee,right=bonus,on='empId',how='left')
+    return df[(df.bonus<1000) | (df.bonus.isna())][['name','bonus']]
